2207ff07ebb27ae3a08d468355167787bc3002c6
skysharkk <sky.light.blr@gmail.com>
2021-04-13 22:17:44 +0300

    feat: implemented text insertion

--- autocad/acad.py
+++ autocad/acad.py
@@ -1,14 +1,15 @@
-from pyautocad import Autocad
 import win32com.client
 import logging
+from comtypes.automation import VARIANT
+from ctypes import byref
 from array import array
 
 logger = logging.getLogger(__name__)
 
 
-class Acad:
-    def __init__(self):
-        self.acad = Autocad(create_if_not_exists=True)
+class AcadUtils:
+    def __init__(self, acad):
+        self.acad = acad
         self.doc = self.acad.doc
         self.shell = win32com.client.Dispatch("WScript.Shell")
         self.selected = None
@@ -42,12 +43,52 @@ class Acad:
         for index in range(int(len(points) / amount_of_coordinates)):
             polyline.SetWidth(index, width, width)
 
-    def draw_table(self, table_coordinates):
+    def draw_table(self, table_coordinates, column_width, text_height, text):
         self.draw_polyline(table_coordinates["title"], 0.6)
         for row in table_coordinates["row"]["coordinates"]:
             self.draw_polyline(row)
-        for separator in table_coordinates["separators"]:
+        for index in range(len(table_coordinates["separators"])):
+            separator = table_coordinates["separators"][index]
+            column_initial_point = array("d", separator[0:3])
             self.draw_polyline(separator, 0.6)
+            self.draw_text(column_initial_point, column_width[index], text_height, "MiddleCenter", text[index])
+
+    @staticmethod
+    def get_bounding_box(entity):
+        min_point = VARIANT(array("d", array("d", [0, 0, 0])))
+        max_point = VARIANT(array("d", array("d", [0, 0, 0])))
+        ref_min_point = byref(min_point)
+        ref_max_point = byref(max_point)
+        entity.GetBoundingBox(ref_min_point, ref_max_point)
+        return [array("d", list(*min_point)), array("d", list(*max_point))]
+
+    @staticmethod
+    def get_mtext_line_height(mode_space, width, text_height, text):
+        text = mode_space.AddMText(array("d", array("d", [0, 0, 0])), width, text)
+        text.Height = text_height
+        bounding_box_coordinates = AcadUtils.get_bounding_box(text)
+        text.Delete()
+        return abs(bounding_box_coordinates[1][1] - bounding_box_coordinates[0][1])
+
+    def draw_text(self, insertion_point, width, height, alignment, text):
+        alignment_dict = {
+            "TopLeft": 1,
+            "TopCenter": 2,
+            "TopRight": 3,
+            "MiddleLeft": 4,
+            "MiddleCenter": 5,
+            "MiddleRight": 6,
+            "BottomLeft": 7,
+            "BottomCenter": 8,
+            "BottomRight": 9,
+        }
+        text = self.doc.ModelSpace.AddMText(insertion_point, width, text)
+        text.Height = height
+        text.AttachmentPoint = alignment_dict[alignment]
+        mtext_height = AcadUtils.get_mtext_line_height(self.doc.ModelSpace, width, height, text)
+
+
+
 
 
 
