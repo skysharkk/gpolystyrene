3fac86a38dab43ac0125d22b2654a98ad2b9170b
skysharkk <sky.light.blr@gmail.com>
2021-04-15 23:06:35 +0300

    feat: implemented method to get data obout selected elements

--- autocad/acad.py
+++ autocad/acad.py
@@ -3,6 +3,7 @@ import logging
 from comtypes.automation import VARIANT
 from ctypes import byref
 from array import array
+from collections import namedtuple
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +13,7 @@ class AcadUtils:
         self.acad = acad
         self.doc = self.acad.doc
         self.shell = win32com.client.Dispatch("WScript.Shell")
-        self.selected = None
+        self.__selected_data = None
 
     def change_focus_window(self):
         self.shell.AppActivate(self.acad.app.Caption)
@@ -21,16 +22,28 @@ class AcadUtils:
         self.change_focus_window()
         self.doc.Utility.prompt(text)
         try:
-            self.doc.SelectionSets.Item("SS1").Delete()
+            if self.doc.SelectionSets.Count > 0:
+                self.doc.SelectionSets.Item("SS1").Delete()
         except AssertionError:
             logger.debug('Delete selection failed')
+        finally:
+            selected = self.doc.SelectionSets.Add("SS1")
+            selected.SelectOnScreen()
+            self.__selected_data = self.get_param_of_selected_el(selected)
 
-        self.selected = self.doc.SelectionSets.Add("SS1")
-        self.selected.SelectOnScreen()
+    def get_selected_data(self):
+        copied_data = [*self.__selected_data]
+        self.__selected_data = None
+        return copied_data
 
-    def print_selected(self):
-        for index in range(self.selected.Count):
-            print(self.selected.Item(index))
+    @staticmethod
+    def get_param_of_selected_el(selected_elements):
+        Data = namedtuple("Data", ["coord", "area", "amount"])
+        result = []
+        for index in range(selected_elements.Count):
+            el = selected_elements.Item(index)
+            result.append(Data(el.Coordinates, el.Area))
+        return result
 
     def get_point(self, message_text="Выберете точку"):
         self.change_focus_window()
@@ -42,16 +55,6 @@ class AcadUtils:
         polyline = self.doc.ModelSpace.AddPolyline(points)
         for index in range(int(len(points) / amount_of_coordinates)):
             polyline.SetWidth(index, width, width)
-
-    def draw_table(self, table_coordinates, column_width, text_height, text):
-        self.draw_polyline(table_coordinates["title"], 0.6)
-        for row in table_coordinates["row"]["coordinates"]:
-            self.draw_polyline(row)
-        for index in range(len(table_coordinates["separators"])):
-            separator = table_coordinates["separators"][index]
-            column_initial_point = array("d", separator[0:3])
-            self.draw_polyline(separator, 0.6)
-            self.draw_text(column_initial_point, column_width[index], text_height, "MiddleCenter", text[index])
 
     @staticmethod
     def get_bounding_box(entity):
@@ -86,10 +89,3 @@ class AcadUtils:
         text.Height = height
         text.AttachmentPoint = alignment_dict[alignment]
         mtext_height = AcadUtils.get_mtext_line_height(self.doc.ModelSpace, width, height, text)
-
-
-
-
-
-
-
