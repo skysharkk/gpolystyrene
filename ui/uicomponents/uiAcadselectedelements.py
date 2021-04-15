cff5ce640af1f7add8e2e69134844757a7028193
skysharkk <sky.light.blr@gmail.com>
2021-04-15 23:04:30 +0300

    feat: implemented opportunity to add information to the table

--- ui/uicomponents/uiAcadselectedelements.py
+++ ui/uicomponents/uiAcadselectedelements.py
@@ -2,17 +2,22 @@ from PyQt6 import QtWidgets
 
 
 class AcadSelectedElements:
-    def __init__(self, table, table_data):
+    def __init__(self, table):
         self.table = table
-        self.table_data = table_data
         self.row_count = 0
+        self.table_data = []
+
+        """
+        table_data 
+        []
+        """
 
     def insert_row(self):
         self.table.insertRow(self.row_count)
         self.row_count += 1
 
-    def add_element(self, data, position):
-        item = QtWidgets.QTableWidgetItem(str(data))
+    def add_element(self, el_data, position):
+        item = QtWidgets.QTableWidgetItem(str(el_data))
         self.table.setItem(position[0], position[1], item)
 
     def add_data(self):
@@ -21,5 +26,5 @@ class AcadSelectedElements:
     def export_table(self):
         pass
 
-    def delete_el(self):
+    def delete_row(self):
         pass
