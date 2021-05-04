3aa02eeabc68d0b38d24019c15760d52917afd07
skysharkk <sky.light.blr@gmail.com>
2021-05-04 23:14:57 +0300

    feat: implemented acad table drawing

--- main.py
+++ main.py
@@ -1,27 +1,86 @@
+from pprint import pprint
+
 from PyQt6.QtWidgets import QApplication
+from pyautocad import Autocad
+
+from ui.uicomponents.uiaddposition import Positions
+from ui.uicomponents.uiscale import Scale
+from ui.uicomponents.uiSelectElements import SelectElements
 from ui.uimain import Ui
 from ui.uicomponents.uiparameters import Parameters
 from ui.uicomponents.uiaddedelements import AddedElements
-from autocad.acad import Acad
-from autocad.table import Table
-
+from autocad.acad import AcadUtils
+from autocad.AutocadTable import AcadTable
+from ui.uicomponents.uiAcadselectedelements import AcadSelectedElements
+from utils.myutils import get_rectangle_sizes, show_error_window
 
 if __name__ == '__main__':
     import sys
 
+    acad = Autocad(create_if_not_exists=True)
+    app = QApplication(sys.argv)
+    window = Ui()
+    window.show()
+    parameters = Parameters(window.lineEdit, window.comboBox, window.pushButton)
+    added_elements = AddedElements(window.treeWidget, window.pushButton_2)
+    select_elements_btn = SelectElements(window.pushButton_3)
+    acad_utils = AcadUtils(acad)
+    ui_table = AcadSelectedElements(window.tableWidget_2, window.pushButton_4, window.pushButton_7)
+    scale = Scale(window.lineEdit_2)
+    pos = Positions(window.pushButton_9, acad)
+
+
+    def format_table_data(data):
+        formatted_list = [
+            ['Поз.', 'Обозначение', 'Наименование', 'Кол.', u'Объем ед.м\u00b3', 'Примечание'],
+            ['', '', 'Плиты пенополистирольные', '', '', ''],
+        ]
+        for item in data:
+            formatted_list.append(item['row'])
+        formatted_list.append(['', '', 'Плиты минераловатные', '', '', ''])
+        formatted_list.append(['', '', '', '', '', ''])
+        formatted_list.append(['', '', '', '', '', ''])
+        formatted_list.append(['', '', '', '', '', ''])
+        return formatted_list
+
+
+    def create_table_data(params, list_data, acad_scale):
+        result = []
+        for item in list_data:
+            sizes = get_rectangle_sizes(item.coord, acad_scale)
+            thickness = params[0]
+            result.append([params[1], [*sizes, int(thickness)], item])
+        return result
+
+
+    def select_items():
+        params = added_elements.get_selected_item_value()
+        scale_value = scale.get_value()
+        if params and scale_value:
+            acad_utils.select_drew_objects()
+            data = acad_utils.get_selected_data()
+            ui_table.add_data_to_table(create_table_data(params, data, scale_value))
+            pos.set_action(ui_table.get_coordinates(), scale.get_value())
+        else:
+            show_error_window('Необходима выбрать толщину и тип утеплителя')
+
+
+    def draw_table():
+        point = acad_utils.get_point()
+        column_width = [600, 2400, 2600, 400, 600, 800]
+        formatted_data = format_table_data(ui_table.table_data)
+        row_height = [600] * len(formatted_data)
+        row_height[1:] = [320] * (len(formatted_data) - 1)
+        table = AcadTable(point, scale.get_value(), column_width, row_height, formatted_data,
+                          acad)
+        table.create_table(60, 100/scale.get_value())
+
 
     def main():
-        app = QApplication(sys.argv)
-        window = Ui()
-        window.show()
-        parameters = Parameters(window.lineEdit, window.comboBox, window.pushButton)
-        added_elements = AddedElements(window.treeWidget, window.pushButton_2)
         parameters.connect_action(added_elements.add_el(parameters.get_entered_data))
-        autocad = Acad()
-        # table = Table(autocad.get_point(), 4, 40)
-        # table_data = table.create_table()
-        # autocad.draw_table(table_data)
+        select_elements_btn.connect_action(select_items)
+        ui_table.draw_table_action(draw_table)
         sys.exit(app.exec())
 
-    main()
 
+    main()
--- ui/uicomponents/uiAcadselectedelements.py
+++ ui/uicomponents/uiAcadselectedelements.py
@@ -1,30 +1,108 @@
 from PyQt6 import QtWidgets
+from utils.myutils import compare_list_or_tuple, get_corner_coordinates, is_elem_exist_in_collection
+from utils.myutils import round_half_up
+from utils.myutils import show_error_window
 
 
 class AcadSelectedElements:
-    def __init__(self, table):
+    def __init__(self, table, delete_btn, draw_table_btn):
+        self.dict_type = {
+            "ППТ-15-А-Р": "СТБ1437",
+            "Эффективный утеплитель λ ≤ 0,034 Вт/(м·°C)": "Эффективный утеплитель λ ≤ 0,034 Вт/(м·°C)"
+        }
+        self.dict_name = {
+            "ППТ-15-А-Р": "ППТ-15-А-Р",
+            "Эффективный утеплитель λ ≤ 0,034 Вт/(м·°C)": ""
+        }
+        self.delete_btn = delete_btn
+        self.draw_table_btn = draw_table_btn
         self.table = table
-        self.row_count = 0
         self.table_data = []
-
-        """
-        table_data 
-        []
-        """
+        self.delete_btn.clicked.connect(self.delete_row)
 
     def insert_row(self):
-        self.table.insertRow(self.row_count)
-        self.row_count += 1
+        self.table.insertRow(len(self.table_data) - 1)
 
-    def add_element(self, el_data, position):
+    def add_elements_to_cell(self, el_data, position):
         item = QtWidgets.QTableWidgetItem(str(el_data))
         self.table.setItem(position[0], position[1], item)
 
-    def add_data(self):
-        pass
+    def add_data_to_row(self, row_data, row_index):
+        for index, item in enumerate(row_data):
+            self.add_elements_to_cell(item, [row_index, index])
+
+    def change_amount(self, row_index):
+        item_value = int(self.table.item(row_index, 3).text())
+        self.add_elements_to_cell(item_value + 1, [row_index, 3])
+
+    def is_object_exist(self, obj_coordinates):
+        for el in self.table_data:
+            if is_elem_exist_in_collection(obj_coordinates, el["acad_data"][2]):
+                show_error_window("Одна из позиций уже был выбран!")
+                return True
+        return False
+
+    def find_if_exist(self, acad_data_el):
+        if len(self.table_data) > 0:
+            for el_index, el in enumerate(self.table_data):
+                if compare_list_or_tuple(acad_data_el[1], el["acad_data"][1]) \
+                        and acad_data_el[0] == el["acad_data"][0]:
+                    return el_index
+        return -1
+
+    def insert_data(self, item_data):
+        ppt_type = item_data[0]
+        sizes = item_data[1]
+        volume = round_half_up((sizes[0] * sizes[1] * sizes[2]) / 1000000000, 2, False)
+        self.table_data.append({
+            "row": [
+                len(self.table_data) + 1,
+                self.dict_type[ppt_type],
+                f"{self.dict_name[ppt_type]} {sizes[0]}x{sizes[1]}x{sizes[2]}",
+                1,
+                volume,
+                "м\u00b3"
+            ],
+            "acad_data": [item_data[0], item_data[1], [item_data[2]]]
+        })
+
+    def add_data_to_table(self, acad_data):
+        for el in acad_data:
+            if not self.is_object_exist(el[2].coord):
+                is_exist = self.find_if_exist(el)
+                if is_exist != -1:
+                    self.table_data[is_exist]["row"][3] += 1
+                    self.table_data[is_exist]["acad_data"][2].append(el[2])
+                    self.change_amount(is_exist)
+                else:
+                    self.insert_data(el)
+                    self.insert_row()
+                    self.add_data_to_row(self.table_data[-1]["row"], len(self.table_data) - 1)
+        self.table.resizeColumnsToContents()
+        self.table.horizontalHeader().setStretchLastSection(True)
 
     def export_table(self):
         pass
 
+    def change_position(self):
+        for index, item in enumerate(self.table_data):
+            item["row"][0] = index + 1
+            self.add_elements_to_cell(item["row"][0], [index, 0])
+
+    def draw_table_action(self, func):
+        self.draw_table_btn.clicked.connect(func)
+
     def delete_row(self):
-        pass
+        row = self.table.currentRow()
+        self.table.removeRow(row)
+        self.table_data.pop(row)
+        self.change_position()
+
+    def get_coordinates(self):
+        result = []
+        for item in self.table_data:
+            position = []
+            for data in item["acad_data"][2]:
+                position.append(get_corner_coordinates(data.coord))
+            result.append(position)
+        return result
