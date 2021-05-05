abdec543cc5030aa75394383acd1761c80cd9000
skysharkk <sky.light.blr@gmail.com>
2021-05-05 20:33:53 +0300

    feat: add greedypacker library

---
+++ ui/uicomponents/uibalance.py
@@ -0,0 +1,10 @@
+
+class Balance:
+    def __init__(self, data):
+        """
+                :param data: [[[sizes], type, count]]
+        """
+        self.data = data
+        self.container_sizes = [2000, 1000]
+
+    def
